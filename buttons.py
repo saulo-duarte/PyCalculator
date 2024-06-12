addd4f9286ee97303942aa3865074f5c4a421b7d
saulo-duarte <saulolduarte@outlook.com>
2024-06-11 21:12:13 -0300

    Botoes respondem ao click

--- buttons.py
+++ buttons.py
@@ -1,11 +1,13 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
 from variables import MEDIUM_FONT_SIZE
 from utils import is_number_or_dot, is_empty
+from display import Display
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
+
 
     def configStyle(self):
         font = self.font()
@@ -13,11 +15,13 @@ class Button(QPushButton):
         font.setFamily('Segoe UI')
         font.setBold(True)
         self.setFont(font)
+        self.setMinimumSize(75, 75)
 
 class ButtonsGrid(QGridLayout):
-    def   __init__ (self, *args, **kwargs):
+    def   __init__ (self, display: Display, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.display = display
         self._gridMasks = [
             ['C', '<', '^', '/'],
             ['7', '8', '9', '*'],
@@ -34,5 +38,15 @@ class ButtonsGrid(QGridLayout):
                         button.setProperty('cssClass', 'specialButton')
 
                     self.addWidget(button, rowNumber, colNumber)
+                    buttonSlot = self._makeButtonDisplaySlot(
+                        self._insertButtonTextToDisplay, button
+                        )
+                    button.clicked.connect(buttonSlot)
 
-    
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        def realSlot(checked):
+            func(checked, *args, **kwargs)
+        return realSlot        
+
+    def _insertButtonTextToDisplay(self, checked, button):
+        self.display.insert(button.text())
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     # Grid
 
-    buttonsGrid = ButtonsGrid()
+    buttonsGrid = ButtonsGrid(display)
     window.vLayout.addLayout(buttonsGrid)
     
     # Informações
